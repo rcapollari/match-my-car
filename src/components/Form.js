62c980830434af7278f7674ff7677fbd1dc2921c
Rio Capollari <rcapollari@oakland.edu>
2025-02-11 19:54:37 -0500

    add form component

---
+++ src/components/Form.js
@@ -0,0 +1,11 @@
+function Form() {
+  return (
+    <form>
+      <input type="text" placeholder="Name" />
+      <input type="text" placeholder="Email" />
+      <button>Submit</button>
+    </form>
+  );
+}
+
+export default Form;
